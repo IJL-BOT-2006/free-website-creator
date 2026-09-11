c4ddaada582ba75f84316169ecb17d55b469a643
IJL-BOT-2006 <ijlalsouhil@gmail.com>
2026-09-11 05:08:29 +0300

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -101,7 +101,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
         rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Aref+Ruqaa+Ink:wght@400;700&family=Tajawal:wght@400;500;700;800;900&family=IBM+Plex+Sans+Arabic:wght@300;400;500;600&display=swap",
+      href: "https://fonts.googleapis.com/css2?family=katibeh&family=Tajawal:wght@400;500;700;800;900&family=IBM+Plex+Sans+Arabic:wght@300;400;500;600&display=swap",
       },
       { rel: "icon", href: "/favicon.png", type: "image/png" },
     ],
