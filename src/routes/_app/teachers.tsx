3557f43ccc1794675be72e6017aa228ec2c4af99
IJL-BOT-2006 <ijlalsouhil@gmail.com>
2026-09-23 17:00:15 +0300

    Update teachers.tsx

--- src/routes/_app/teachers.tsx
+++ src/routes/_app/teachers.tsx
@@ -10,6 +10,7 @@ import { useAuth } from "@/lib/auth";
 import { useStaff, useTeacherAttendanceRates } from "@/lib/queries";
 import { RateBadge } from "@/components/mini-charts";
 import { TeacherMonthlyDialog } from "@/components/monthly-attendance-dialog";
+import { TeacherMonthlyDialog } from "@/components/monthly-attendance-dialog";
 import { changeAccountRole, createAccount, resetAccountPassword } from "@/lib/admin.functions";
 import {
   ACCOUNT_STATUS_LABELS,
